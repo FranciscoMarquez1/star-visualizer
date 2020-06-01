cbe318bca36e415ca70b040493436323058be44d
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-06-01 17:23:45 -0500

    Add background music

--- proyecto-final/js/main.js
+++ proyecto-final/js/main.js
@@ -65,7 +65,20 @@ function main()
 	] );
     scene.add(camera);
 
+    var listener = new THREE.AudioListener();
+    camera.add( listener );
 
+    // create a global audio source
+    var sound = new THREE.Audio( listener );
+
+    // load a sound and set it as the Audio object's buffer
+    var audioLoader = new THREE.AudioLoader();
+    audioLoader.load( '../sounds/fondo.mp3', function( buffer ) {
+      sound.setBuffer( buffer );
+      sound.setLoop( true );
+      sound.setVolume( 0.2 );
+      sound.play();
+    });
 
     // GUI
     gui = new dat.GUI();
